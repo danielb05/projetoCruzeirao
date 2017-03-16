3abc9354a6dc4e8d44c8ed56376666b9c90a6f84
132118 <132118@IA21.labinfo.facens.br>
2017-03-15 22:37:55 -0300

    codProf

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,27 @@
+package sistema.modelos;
+
+public class Professor {
+
+	private int codProf;
+	private String nome;
+	private String disciplina;
+	
+	public int getCodProf() {
+		return codProf;
+	}
+	public void setCodProf(int codProf) {
+		this.codProf = codProf;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+}
